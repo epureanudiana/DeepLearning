62c4bac0d72f64c2ff737f1ac852a5d14b5c9a36
Diana <epureanudiana13@gmail.com>
2018-03-25 19:47:27 +0200

    Added training for the model.

--- src/main/java/experiments/FunctionExperiment.java
+++ src/main/java/experiments/FunctionExperiment.java
@@ -6,6 +6,15 @@
 package experiments;
 import nl.tue.s2id90.dl.experiment.Experiment;
 import java.io.IOException;
+import nl.tue.s2id90.dl.NN.Model;
+import nl.tue.s2id90.dl.NN.initializer.Gaussian;
+import nl.tue.s2id90.dl.NN.layer.InputLayer;
+import nl.tue.s2id90.dl.NN.layer.SimpleOutput;
+import nl.tue.s2id90.dl.NN.loss.Loss;
+import nl.tue.s2id90.dl.NN.optimizer.Optimizer;
+import nl.tue.s2id90.dl.NN.optimizer.SGD;
+import nl.tue.s2id90.dl.NN.tensor.TensorShape;
+import nl.tue.s2id90.dl.NN.validate.Regression;
 import nl.tue.s2id90.dl.input.GenerateFunctionData;
 import nl.tue.s2id90.dl.input.InputReader;
 
@@ -16,6 +25,8 @@ import nl.tue.s2id90.dl.input.InputReader;
 public class FunctionExperiment extends Experiment{
   // ( hyper ) parameters
     int batchSize = 32;
+    int epochs = 10;
+    float learningRate = 0.01f;
     
     public void go() throws IOException {
     // you are going to add code here.
@@ -23,12 +34,32 @@ public class FunctionExperiment extends Experiment{
     InputReader reader = GenerateFunctionData.
             THREE_VALUED_FUNCTION ( batchSize ) ;
     System.out.println("Reader info:\n" + reader.toString());
+    int inputs = reader.getInputShape().getNeuronCount();
+    int outputs = reader.getOutputShape().getNeuronCount();
     reader.getValidationData(10).forEach(System.out::println);
+    Model m = createModel(inputs, outputs);
+    System.out.print(m);
+    m.initialize (new Gaussian());
+    // Training : create and configure SGD && train model
+    Optimizer sgd = SGD.builder(). model( m ).validator(new Regression()).
+            learningRate(learningRate).build() ;
+    trainModel(m, reader, sgd, epochs, 0);
     }
+    
+    Model createModel ( int inputs , int outputs ) {
+    Model model = new Model(new InputLayer("In", new TensorShape(inputs), true)); 
+    model.addLayer(new SimpleOutput("Out", new TensorShape(inputs), outputs, newMSE(), true));
+    System.out.println(model); 
+    return model ;
+}
     
     public static void main(String[] args) throws IOException {
         new FunctionExperiment().go();
    }
+
+    private Loss newMSE() {
+       return null;
+    }
 }
     
 
